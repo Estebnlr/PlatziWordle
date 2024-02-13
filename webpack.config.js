19d4969aba4a74759363384f56a27b0509dab45f
js-portfolio <cidorbleit@gmail.com>
2024-02-13 10:55:45 -0500

    go

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,37 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['*','.mjs','.js','.svelte']
-  },
-  mode: "development"
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js'
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          }
+        }
+      ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin({
+          patterns: [
+            { from: './public/style.css', to: '' }
+          ]
+        })
+    ]
 };
